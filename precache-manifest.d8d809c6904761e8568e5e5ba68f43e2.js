281b286deaacd9b69e793048a8c23d047eba6937
SG0307799 <kartik.madaan@sabre.com>
2020-05-18 16:30:17 +0530

    Updates

---
+++ precache-manifest.d8d809c6904761e8568e5e5ba68f43e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a27290595a45dd4f77625ddd8fdcca",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2fb00ad9de34af1149be",
+    "url": "/tic-tac-toe/static/css/main.a2765206.chunk.css"
+  },
+  {
+    "revision": "1f503cb2fd0da2e9d1ea",
+    "url": "/tic-tac-toe/static/js/2.714c5793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.714c5793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb00ad9de34af1149be",
+    "url": "/tic-tac-toe/static/js/main.6c12ee31.chunk.js"
+  },
+  {
+    "revision": "b02c5eb9981aefa3cb03",
+    "url": "/tic-tac-toe/static/js/runtime-main.08e76764.js"
+  }
+]);
